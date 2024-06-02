d8d8758d0063f37071205dd7f75d11d33aee665a
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2024-06-02 18:05:29 +0530

    .

--- vidixer_project/vidixerapp/urls.py
+++ vidixer_project/vidixerapp/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/', views.user_home, name='home'),
+    path('home/', views.user_home, name='user_home'),
     path('home/upload/', views.video_upload, name='video_upload'),
-    path('login/', LoginView.as_view(template_name='login.html',next_page='home'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html',next_page='home'), name='logout'),
+    path('login/', LoginView.as_view(template_name='login.html',next_page='home/'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html',next_page=''), name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
