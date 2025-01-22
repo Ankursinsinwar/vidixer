130cbbdafb22d513d9060fc4a77921bdebf3fc02
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2025-01-22 19:41:56 +0530

    till upload and view the video

--- vidixer_project/vidixerapp/forms.py
+++ vidixer_project/vidixerapp/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from .models import Video
 
+# class VideoForm(forms.ModelForm):
+#     class Meta:
+#         model = Video
+#         fields = ['title', 'description', 'uploaded_by', 'video_file']
+#         widgets = {
+#             'uploaded_by': forms.HiddenInput(),
+#         }
+
+
 class VideoForm(forms.ModelForm):
     class Meta:
         model = Video
-        fields = ['title', 'description', 'uploaded_by', 'video_file']
-        widgets = {
-            'uploaded_by': forms.HiddenInput(),
-        }
+        fields = ['title', 'description', 'video_file']  # Exclude 'uploaded_by'
