87ba994f8faefd56ba0693514e5c9979c823010b
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2025-07-05 13:52:40 +0530

    Added and protected JWT login/register views And Added role-based access control (client vs editor permissions)

--- backend/vidixer/urls.py
+++ backend/vidixer/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
-    # path('api/users/', include('users.urls')),  # add later
+    path('api/users/', include('users.urls')),
 ]
 
 if settings.DEBUG:
